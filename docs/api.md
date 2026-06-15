e1fa11b1f4c0743afe1e4666171dd4cb84d70dc4
HugoHakem <hakem@ebi.ac.uk>
2026-06-15 16:52:09 +0100

    docs: add sphinx setup inspired from scverse

---
+++ docs/api.md
@@ -0,0 +1,113 @@
+# API
+
+## Core types
+
+```{eval-rst}
+.. module:: scperteval.types
+.. currentmodule:: scperteval.types
+
+.. autosummary::
+    :toctree: generated
+
+    RunConfig
+    Protocol
+    Calibrator
+```
+
+## Runner
+
+```{eval-rst}
+.. module:: scperteval.runner
+.. currentmodule:: scperteval.runner
+
+.. autosummary::
+    :toctree: generated
+
+    run_protocol
+```
+
+## Protocols
+
+```{eval-rst}
+.. module:: scperteval.protocols
+.. currentmodule:: scperteval.protocols
+
+.. autosummary::
+    :toctree: generated
+
+    PROTOCOLS
+    GROUPS
+    TABLE
+```
+
+### Metrics
+
+```{eval-rst}
+.. module:: scperteval.protocols.metrics
+.. currentmodule:: scperteval.protocols.metrics
+
+.. autosummary::
+    :toctree: generated
+```
+
+## Calibrators
+
+```{eval-rst}
+.. module:: scperteval.calibrators
+.. currentmodule:: scperteval.calibrators
+
+.. autosummary::
+    :toctree: generated
+```
+
+## Building blocks
+
+### Differential expression
+
+```{eval-rst}
+.. module:: scperteval.blocks.de
+.. currentmodule:: scperteval.blocks.de
+
+.. autosummary::
+    :toctree: generated
+```
+
+### Feature spaces
+
+```{eval-rst}
+.. module:: scperteval.blocks.spaces
+.. currentmodule:: scperteval.blocks.spaces
+
+.. autosummary::
+    :toctree: generated
+```
+
+## Registry
+
+```{eval-rst}
+.. module:: scperteval.registry
+.. currentmodule:: scperteval.registry
+
+.. autosummary::
+    :toctree: generated
+
+    Registry
+```
+
+## Dataset & I/O
+
+```{eval-rst}
+.. module:: scperteval.dataset
+.. currentmodule:: scperteval.dataset
+
+.. autosummary::
+    :toctree: generated
+```
+
+```{eval-rst}
+.. module:: scperteval.io
+.. currentmodule:: scperteval.io
+
+.. autosummary::
+    :toctree: generated
+```
---
+++ docs/conf.py
@@ -0,0 +1,121 @@
+# Configuration file for the Sphinx documentation builder.
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import shutil
+import sys
+from datetime import datetime
+from importlib.metadata import metadata
+from pathlib import Path
+
+from sphinxcontrib import katex
+
+HERE = Path(__file__).parent
+sys.path.insert(0, str(HERE / "extensions"))
+
+# -- Project information -----------------------------------------------------
+
+info = metadata("scperteval")
+project = info["Name"]
+author = info.get("Author") or "scPertEval authors"
+copyright = f"{datetime.now():%Y}, {author}"
+version = info["Version"]
+_project_urls = info.get_all("Project-URL") or []
+urls = dict(pu.split(", ", 1) for pu in _project_urls)
+repository_url = urls.get("Source", "https://github.com/Virtual-Cell-Research-Community/scPertEval")
+
+release = info["Version"]
+
+bibtex_bibfiles = ["references.bib"]
+templates_path = ["_templates"]
+nitpicky = True
+needs_sphinx = "4.0"
+
+html_context = {
+    "display_github": True,
+    "github_user": "Virtual-Cell-Research-Community",
+    "github_repo": "scPertEval",
+    "github_version": "main",
+    "conf_py_path": "/docs/",
+}
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "myst_nb",
+    "sphinx_copybutton",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib.bibtex",
+    "sphinxcontrib.katex",
+    "sphinx_autodoc_typehints",
+    "sphinx_design",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "sphinxext.opengraph",
+    *[p.stem for p in (HERE / "extensions").glob("*.py")],
+]
+
+autosummary_generate = True
+autodoc_member_order = "groupwise"
+default_role = "literal"
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_use_rtype = True
+napoleon_use_param = True
+myst_heading_anchors = 6
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_image",
+    "html_admonition",
+]
+myst_url_schemes = ("http", "https", "mailto")
+nb_output_stderr = "remove"
+nb_execution_mode = "off"
+nb_merge_streams = True
+typehints_defaults = "braces"
+always_use_bars_union = True
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".ipynb": "myst-nb",
+    ".myst": "myst-nb",
+}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "anndata": ("https://anndata.readthedocs.io/en/stable/", None),
+    "scanpy": ("https://scanpy.readthedocs.io/en/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+}
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+
+html_title = project
+
+html_theme_options = {
+    "repository_url": repository_url,
+    "use_repository_button": True,
+    "path_to_docs": "docs/",
+    "navigation_with_keys": False,
+}
+
+pygments_style = "default"
+katex_prerender = shutil.which(katex.NODEJS_BINARY) is not None
+
+nitpick_ignore = [ # type: ignore
+    # Add exceptions here for links outside your control that fail to resolve
+    #     ("py:class", "igraph.Graph"),
+]
